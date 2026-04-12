de3218f298638a911ea1e0cce366c36e651f24d4
Don <udushka@gmail.com>
2026-04-12 11:32:30 +0000

    I modified the files and commented out the use of the CAPTCHA, as it wasn't passing.

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,23 +1,80 @@
-print("Hello world")
-
 from flask import Flask, render_template
+from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
+import os
+import net as neuronet
 
 app = Flask(__name__)
 
-#декоратор для вывода страницы по умолчанию
+# Конфигурация приложения
+app.config['SECRET_KEY'] = 'secret'
+app.config['RECAPTCHA_USE_SSL'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lf9mrMsAAAAAARtdfhNhji_KW0Sto0dpCnzYkIs'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lf9mrMsAAAAAB_hpIe2ZQHmLdM2J5lM6F2ZUmT8'
+app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
+
+bootstrap = Bootstrap(app)
+
+
+class NetForm(FlaskForm):
+    openid = StringField('openid', validators=[DataRequired()])
+    upload = FileField(
+        'Load image',
+        validators=[
+            FileRequired(),
+            FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
+        ]
+    )
+    # recaptcha = RecaptchaField()
+    submit = SubmitField('send')
+
+
 @app.route("/")
 def hello():
-	return " <html><head></head> <body> Hello World! </body></html>"
+    return "<html><head></head><body>Hello World!</body></html>"
 
-#наша новая функция сайта
+
 @app.route("/data_to")
 def data_to():
-	#создаем переменные с данными для передачи в шаблон
-	some_pars = {'user':'Ivan','color':'red'}
-	some_str = 'Hello my dear friends!'
-	some_value = 10
-	#передаем данные в шаблон и вызываем его
-	return render_template('simple.html',some_str = some_str, some_value = some_value,some_pars=some_pars)
+    some_pars = {'user': 'Ivan', 'color': 'red'}
+    some_str = 'Hello my dear friends!'
+    some_value = 10
+    return render_template(
+        'simple.html',
+        some_str=some_str,
+        some_value=some_value,
+        some_pars=some_pars
+    )
+
+
+@app.route("/net", methods=['GET', 'POST'])
+def net():
+    form = NetForm()
+    filename = None
+    neurodic = {}
+
+    if form.validate_on_submit():
+        filename = os.path.join(
+            './static',
+            secure_filename(form.upload.data.filename)
+        )
+        fcount, fimage = neuronet.read_image_files(10, './static')
+        decode = neuronet.getresult(fimage)
+        for elem in decode:
+            neurodic[elem[0][1]] = elem[0][2]
+        form.upload.data.save(filename)
+
+    return render_template(
+        'net.html',
+        form=form,
+        image_name=filename,
+        neurodic=neurodic
+    )
+
 
 if __name__ == "__main__":
-	app.run(host='127.0.0.1',port=5000)
+    app.run(host='127.0.0.1', port=5000)
--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,4 @@
 sudo apt-get update && sudo apt-get install python3-pip python3-dev python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
-pip install -U flask gunicorn requests
+pip install -U flask gunicorn requests flask-bootstrap flask-wtf pillow tensorflow keras
