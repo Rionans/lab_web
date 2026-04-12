846a4bfa41d97ff91bfe31d011aad620753fe55b
Don <udushka@gmail.com>
2026-04-12 12:07:23 +0000

    The conditions up to Point 2.11 have been met

--- start_server.sh
+++ start_server.sh
@@ -4,3 +4,5 @@ sudo apt-get update && sudo apt-get install python3-pip python3-dev python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip install -U flask gunicorn requests flask-bootstrap flask-wtf pillow tensorflow keras lxml
+
+# выполнены условия до 2.11 пункта
